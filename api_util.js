ce36e897e6463869892985eeb8508c2c3e983aea
Jimmy Li <jimmyli921@gmail.com>
2017-12-19 20:39:45 -0800

    add chooseOfficial and promptMessage

--- api_util.js
+++ api_util.js
@@ -4,18 +4,30 @@ var fetch = require('node-fetch');
 const apiURL = "https://www.googleapis.com/civicinfo/v2/representatives";
 const apiKey = "AIzaSyDqTXtHz-Crg8vKVakUQqZLldFp_k-X9fM";
 
-const getRepList = (userInput) => {
+function getRepList(userInput) {
   const {address1, address2, city, state, country, zipcode, message} = userInput;
   const fullAddress = [address1,address2,city,state,country,zipcode].join("%20");
   const roles = ["legislatorLowerBody", "legislatorUpperBody"].join("&roles=");
   return fetch(`${apiURL}?key=${apiKey}&address=${fullAddress}&roles=${roles}`)
     .then(checkStatus)
     .then(parseJSON)
-    .then(json => json)
+    .then(createRepList)
     .catch(err => {
       throw err;
     });
-};
+}
+
+function createRepList(json) {
+  const {offices, officials} = json;
+  let repList = [];
+  offices.forEach(office => {
+    office.officialIndices.forEach(idx => {
+      const rep = Object.assign({}, officials[idx], {office: office.name});
+      repList.push(rep);
+    });
+  });
+  return repList;
+}
 
 function checkStatus(response) {
   if (response.status >= 200 && response.status < 300) {
--- index.js
+++ index.js
@@ -3,36 +3,36 @@ var commander = require('commander');
 var getRepList = require('./api_util');
 
 const questions = [
-  {
-    name: 'name',
-    type: 'input',
-    message: 'From Name:'
-  },
-  {
-    name: 'address1',
-    type: 'input',
-    message: 'From Address Line 1:'
-  },
-  {
-    name: 'address2',
-    type: 'input',
-    message: 'From Address Line 2:'
-  },
-  {
-    name: 'city',
-    type: 'input',
-    message: 'From City:'
-  },
-  {
-    name: 'state',
-    type: 'input',
-    message: 'From State:'
-  },
-  {
-    name: 'country',
-    type: 'input',
-    message: 'From Country:'
-  },
+  // {
+  //   name: 'name',
+  //   type: 'input',
+  //   message: 'From Name:'
+  // },
+  // {
+  //   name: 'address1',
+  //   type: 'input',
+  //   message: 'From Address Line 1:'
+  // },
+  // {
+  //   name: 'address2',
+  //   type: 'input',
+  //   message: 'From Address Line 2:'
+  // },
+  // {
+  //   name: 'city',
+  //   type: 'input',
+  //   message: 'From City:'
+  // },
+  // {
+  //   name: 'state',
+  //   type: 'input',
+  //   message: 'From State:'
+  // },
+  // {
+  //   name: 'country',
+  //   type: 'input',
+  //   message: 'From Country:'
+  // },
   {
     name: 'zipcode',
     type: 'input',
@@ -40,23 +40,44 @@ const questions = [
   }
 ];
 
-const selectSenator = [
-  {
-    name: 'senators',
-    type: 'list',
-    message: 'Which official would you like to send an email to?',
-    choices: ["pizza"]
-  }
-];
+let repList = [];
+let messageObj = {};
 
-console.log("Please enter your address");
-inquirer.prompt(questions).then(answers =>
+console.log("Please enter your information");
+inquirer.prompt(questions).then(answers => {
+  messageObj = Object.assign(messageObj, {"from": answers});
   getRepList(answers)
-    .then(json => createRepList(json))
-    .then(() => inquirer.prompt(selectSenator))
-    .catch(err => console.log(err))
-);
+    .then(list => {
+      repList = list;
+      chooseOfficial(list)
+        .then(answer => {
+        const official = repList.filter(rep => {
+          return rep.name === answer.senator.split(" - ")[0];
+        })[0];
+        messageObj = Object.assign(messageObj, {"to": official});
+        promptMessage().then(answer2 => {
+          console.log(messageObj);
+        });
+      });
+    })
+    .catch(err => console.log(err));
+});
 
-const createRepList = (json) => {
+function chooseOfficial(list){
+  return inquirer.prompt({
+    name: 'senator',
+    type: 'list',
+    message: 'Which official would you like to send a message to?',
+    choices: list.map(official => {
+      return official.name + " - " + official.office;
+    })
+  });
+}
 
-};
+function promptMessage() {
+  return inquirer.prompt({
+    name: 'message',
+    type: 'input',
+    message: 'Enter your message to send:'
+  });
+}
